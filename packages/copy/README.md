83aa17ee137e80c6b2983171d7fc44174104f054
mtsmachado8 <mtsmachado8@gmail.com>
2019-11-05 10:15:44 -0300

    chore: test

---
+++ packages/copy/README.md
@@ -0,0 +1,16 @@
+# `logger`
+
+Módulo usado para logs da aplicação
+
+## Usage
+
+```
+const logger = require('logger');
+logger.info('message')
+```
+
+## Levels
+- log
+- info
+- warn
+- error
---
+++ packages/copy/src/index.js
@@ -0,0 +1,58 @@
+/* global BigInt */
+const { hostname } = require('os')
+const { createLogger, transports, format } = require('winston')
+const { combine, colorize, printf, timestamp, errors, json } = format
+
+const LEVEL = process.env.LOG_LEVEL
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+const colorFormat = combine(
+  errors({ stack: true }),
+  colorize(),
+  printf(logMessage => `${logMessage.level}: ${logMessage.message}`)
+)
+const jsonFormat = combine(
+  errors({ stack: true }),
+  timestamp(),
+  json()
+)
+
+const logger = createLogger({
+  level: LEVEL || 'info',
+  format: (NODE_ENV === 'development' || NODE_ENV === 'test') ? colorFormat : jsonFormat,
+  defaultMeta: {
+    hostname: hostname()
+  },
+  transports: [
+    new transports.Console({
+      handleExceptions: true
+    })
+  ]
+})
+
+logger.stream = {
+  write: message => {
+    logger.info(message)
+  }
+}
+
+logger.startTracer = (traceName, debug = logger.debug) => {
+  const parse = v => `${(v / BigInt(1000000))}ms`
+  const start = process.hrtime.bigint()
+  let index = 1
+  let total = BigInt(0)
+  const msg = msg => debug({ message: `${traceName}: msg` })
+
+  msg('started')
+
+  return {
+    step: (description) => {
+      const diff = process.hrtime.bigint() - start - total
+      total += diff
+
+      msg(`step[${index++}]: ${description} time: ${parse(diff)} total: ${parse(total)}`)
+    }
+  }
+}
+
+module.exports = logger
